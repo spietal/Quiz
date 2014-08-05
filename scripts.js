d56f7134b6e4db1bd5cc2dc7f1f3f235d2b3184d
Stephanie Pietal <spietal@gmail.com>
2014-08-05 12:35:02 -0400

    add Ids to quizzes

--- scripts.js
+++ scripts.js
@@ -1,7 +1,28 @@
 $(function() {
-
-  $.get('quiztemplate.html', function(template) {
-    	var rendered = Mustache.render(template, QUIZ);
+    
+	$.get('quiztemplate.html', function(template) {
+		var quiz = addIdsToQuiz(QUIZ);
+    	var rendered = Mustache.render(template, quiz);
     	$('#quiz-container').html(rendered);
-  });
+
+  	});
+
+
+ 
+	var addIdsToQuiz = function(quiz) { 
+		var id = 1;
+		for (var questionIdx = 0; questionIdx < quiz.length; questionIdx++) {
+			var question = quiz[questionIdx];
+			question.qId = id;
+			id += 1;
+			for (var answerIdx = 0; answerIdx < question.answers.length; 
+					answerIdx++) {
+				var answer = question.answers[answerIdx];
+				answer.aId = id;
+				id += 1;
+			}
+		}
+		return quiz;
+	}
+
 });
