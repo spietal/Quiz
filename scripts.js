634bcb25624fc686ae8da6c6f1157ad90c010990
Stephanie Pietal <spietal@gmail.com>
2014-08-05 13:39:59 -0400

    add scoring

--- scripts.js
+++ scripts.js
@@ -23,6 +23,68 @@ $(function() {
 			}
 		}
 		return quiz;
-	}
+	};
 
+
+
+
+
+	var getSubmittedAnswerId = function(question) {
+		return $("input:radio[name=" + question.qId + "]:checked").val();
+	};
+
+
+	var getAnswerById = function(id, question) {
+		for (var answerIdx = 0; answerIdx < question.answers.length; 
+				answerIdx++) {
+			var answer = question.answers[answerIdx];
+			if (answer.aId == id) {
+				return answer;
+			}
+		}
+	};
+
+
+	var isCorrectAnswer = function(answer) {
+		if (answer.isCorrect == null) {
+			return false;
+		}
+		return answer.isCorrect
+	};
+
+
+	var questionIsCorrect = function(question) {
+		var submittedAnswerId = getSubmittedAnswerId(question);
+		if (submittedAnswerId == null) {
+			return false;
+		}
+		var submittedAnswer = getAnswerById(submittedAnswerId, question);
+		var isCorrect = isCorrectAnswer(submittedAnswer);
+		return isCorrect;
+	};
+
+
+	var gradeQuiz = function(quiz) {
+		var numCorrect = 0;
+		for (var i = 0; i < quiz.length; i++) {
+
+			var question = quiz[i];
+			var isCorrect = questionIsCorrect(question);
+			if (isCorrect) {
+				numCorrect += 1;
+			}
+			// console.log("question: " + i);
+			// console.log("isCorrect: " + isCorrect);
+		}
+		var score = numCorrect/quiz.length;
+		score *= 100;
+		score = Math.round(score);
+		console.log(score);
+		$("#quiz-score").text("Your score is "+score+"%");
+	};
+
+
+	$("#submit-button").click(function() {
+		gradeQuiz(QUIZ);
+	});
 });
